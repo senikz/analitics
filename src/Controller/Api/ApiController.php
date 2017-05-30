a6f361f2d57de002ee67c2a796a482a8594196ad
Sergey Zolotukhin <serjikz123@gmail.com>
2017-05-30 19:55:08 +0300

    headers

--- src/Controller/Api/ApiController.php
+++ src/Controller/Api/ApiController.php
@@ -13,7 +13,7 @@ class ApiController extends Controller
 		$this->autoRender = false;
 
         $this->response->header('Access-Control-Allow-Origin', '*');
-        $this->response->header('Access-Control-Allow-Headers', 'user_token');
+        $this->response->header('Access-Control-Allow-Headers', 'user_token, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 		$this->response->header('Content-Type', 'application/json');
 
 		if($this->request->is('options')) {
