5fd7f89b46c39cf25c08eb406f62be863382fdea
Sergey Zolotukhin <serjikz123@gmail.com>
2017-07-24 21:24:52 +0300

    методы создания/удаления сайтов

--- src/Controller/Api/SitesController.php
+++ src/Controller/Api/SitesController.php
@@ -3,6 +3,12 @@ namespace App\Controller\Api;
 
 class SitesController extends ApiController
 {
+	public function initialize()
+	{
+		parent::initialize();
+		$this->loadComponent('Validator');
+	}
+
 	public function index()
     {
 		$result = [];
@@ -14,6 +20,7 @@ class SitesController extends ApiController
 		foreach ($query as $row) {
 			$result[] = [
 				'id' => $row->id,
+				'project_id' => $row->project_id,
 				'domain' => $row->domain,
 			];
 		}
@@ -29,9 +36,44 @@ class SitesController extends ApiController
 
 		$result = [
 			'id' => $site->id,
+			'project_id' => $site->project_id,
 			'caption' => $site->domain,
 		];
 
 		$this->sendData($result);
+    }
+
+	public function add()
+    {
+        if ($this->request->is('post')) {
+            $data = $this->request->getData();
+
+            if ($this->Validator->required($data, ['project_id', 'domain'])) {
+                $site = $this->Sites->newEntity();
+                $ite = $this->Sites->patchEntity($site, $data);
+
+                if ($this->Sites->save($ite)) {
+                    $this->sendData([
+                        'id' => $site->id
+                    ]);
+                }
+
+                $this->sendError($this->Validator->getLastError(__('Can`t add site')));
+            }
+
+            $this->sendError($this->Validator->getLastError());
+        }
+    }
+
+    public function delete($id = null)
+    {
+        if ($this->request->is('delete') && $id) {
+            $site = $this->Sites->get($id);
+            if ($this->Sites->delete($site)) {
+                $this->sendData([]);
+            } else {
+                $this->sendError($this->Validator->getLastError(__('Can`t delete site')));
+            }
+        }
     }
 }
