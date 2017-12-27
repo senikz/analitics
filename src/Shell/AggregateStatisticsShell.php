4ef0f7cc951e4294e9881fc2dd279ddb6373689b
Sergey Zolotukhin <serjikz123@gmail.com>
2017-12-27 23:13:01 +0200

    ihsdriugs

--- src/Shell/AggregateStatisticsShell.php
+++ src/Shell/AggregateStatisticsShell.php
@@ -18,88 +18,52 @@ class AggregateStatisticsShell extends \Cake\Console\Shell
         $this->AdGroups = TableRegistry::get('AdGroups');
         $this->AdGroupStatisticsDaily = TableRegistry::get('AdGroupStatisticsDaily');
 
-		$this->SiteCalls = TableRegistry::get('SiteCalls');
-		$this->SiteEmails = TableRegistry::get('SiteEmails');
+        $this->SiteCalls = TableRegistry::get('SiteCalls');
+        $this->SiteEmails = TableRegistry::get('SiteEmails');
     }
 
     public function yesterday()
     {
-		$emails = $this->SiteEmails->find('all')->all();
-
-		foreach($emails as $email) {
-
-			if($details = $email->getContentDetails()) {
-				if(!empty($details['phrase_id'])) {
-					$keyword = $this->Keywords->find('all')->where(['rel_id' => $details['phrase_id']])->first();
-					if(!empty($keyword)) {
-						$email->keyword_id = $keyword->id;
-						$email->ad_group_id = $keyword->ad_group_id;
-						$email->campaign_id = $keyword->campaign_id;
-					}
-				}
-				$this->SiteEmails->save($email);
-			}
-
-		}
-
-		return;
-		$this->forDate(date('Y-m-d'));
+        $this->forDate(date('Y-m-d'));
     }
 
     public function today()
     {
-		$this->forDate(date('Y-m-d', strtotime('-1 day')));
+        $this->forDate(date('Y-m-d', strtotime('-1 day')));
     }
 
-	private function forDate($date)
-	{
-		$from = $date . ' 00:00:00';
-		$to = $date . ' 23:59:59';
+    private function forDate($date)
+    {
+        $from = $date . ' 00:00:00';
+        $to = $date . ' 23:59:59';
 
-		$adGroups = $this->AdGroups->find('all')
-			->where(['id' => 2780])
-		->all();
-		foreach($adGroups as $adGroup) {
+        $adGroups = $this->AdGroups->find('all')
+            ->where(['id' => 2780])
+        ->all();
+        foreach ($adGroups as $adGroup) {
+            $record = $this->AdGroupStatisticsDaily->find('all')
+                ->where([
+                    'ad_group_id' => $adGroup->id,
+                    'date' => $date,
+                ])
+                ->first();
 
-			$record = $this->AdGroupStatisticsDaily->find('all')
-				->where([
-					'ad_group_id' => $adGroup->id,
-					'date' => $date,
-				])
-				->first();
+            if (empty($record)) {
+                $record = $this->AdGroupStatisticsDaily->newEntity();
+                $record->ad_group_id = $adGroup->id;
+                $record->date = $date;
+            }
 
-			if(empty($record)) {
-				$record = $this->AdGroupStatisticsDaily->newEntity();
-				$record->ad_group_id = $adGroup->id;
-				$record->date = $date;
-			}
+            $conditions = [
+                'ad_group_id' => $adGroup->id,
+                'time >=' => $from,
+                'time <=' => $to,
+            ];
 
-			$campaign = $this->Campaigns->find('all')->where(['Campaigns.id' => $adGroup->campaign_id])->contain(false)->first();
-			$keywords = $this->Keywords->find('all')->select(['rel_id', 'keyword'])->where(['campaign_id' => $adGroup->campaign_id,])->toArray();
+            $record->calls = $this->SiteCalls->findCountBy($conditions);
+            $record->emails = $this->SiteCalls->findCountBy($conditions);
 
-			if(empty($campaign) || empty($keywords)) {
-				continue;
-			}
-
-			$record->calls = $this->SiteCalls->findCountBy([
-				'utm_campaign LIKE' => '%' . $campaign->rel_id . '%',
-				'utm_term IN' => array_map(function($item) {
-					return $item->keyword;
-				}, $keywords),
-				'time >=' => $from,
-				'time <=' => $to,
-			]);
-			$record->emails = $this->SiteCalls->findCountBy([
-				'utm_campaign LIKE' => '%' . $campaign->rel_id . '%',
-				'utm_term REGEXP' => join('|', array_map(function($item) {
-					return $item->rel_id;
-				}, $keywords)),
-				'time >=' => $from,
-				'time <=' => $to,
-			]);
-
-			$this->AdGroupStatisticsDaily->saveStatistics($record);
-		}
-	}
-
+            $this->AdGroupStatisticsDaily->saveStatistics($record);
+        }
+    }
 }
